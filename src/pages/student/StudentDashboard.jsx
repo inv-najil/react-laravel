a0a03c0dc574bb7f944edd14286ab93a8c1d051c
inv-najil <najil.arfak@innovaturelabs.com>
2025-08-01 10:33:54 +0530

    feat:student dashboard with their own details

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -1,15 +1,57 @@
-import { Button, Typography, Stack } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import {
+  Typography,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@mui/material";
+import { getStudentProfile } from "../../api/authService";
+import { useEffect, useState } from "react";
 
-export default function StudetDashboard() {
-  const navigate = useNavigate();
-  
+export default function StudentDashboard() {
+  const [student, setStudent] = useState(null);
+
+  useEffect(() => {
+    getStudentProfile()
+      .then(res => {
+        setStudent(res.data);
+      })
+      .catch(err => {
+        console.error("Failed to fetch", err);
+        alert("Failed to fetch student profile");
+      });
+  }, []);
 
   return (
-    <>
-      <Typography variant="h4" gutterBottom>
-        Welcome Student
-      </Typography>
-    </>
+    <TableContainer component={Paper} sx={{ overflowX: "auto", p: 2 }}>
+      {student && (
+        <Typography variant="h5" sx={{ mb: 3 }}>
+          Hi {student.first_name}
+        </Typography>
+      )}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell><strong>Name</strong></TableCell>
+            <TableCell><strong>Roll Number</strong></TableCell>
+            <TableCell><strong>Email</strong></TableCell>
+            <TableCell><strong>Class</strong></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {student && (
+            <TableRow key={student.id}>
+              <TableCell>{student.first_name} {student.last_name}</TableCell>
+              <TableCell>{student.roll_num}</TableCell>
+              <TableCell>{student.email}</TableCell>
+              <TableCell>{student.class_grade}</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
