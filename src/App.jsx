eabdd2350ca0e4231e2b6a28106f5e68672ac0ae
inv-najil <najil.arfak@innovaturelabs.com>
2025-08-14 18:03:45 +0530

    style:added loader for api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import './App.css'
+import { useState, useEffect } from "react";
+import { registerLoadingSetter } from "./api/axios";
+import { CircularProgress, Backdrop } from "@mui/material";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    registerLoadingSetter(setLoading);
+  }, [])
   return (
+    <>
+      <Backdrop
+        open={loading}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <RouterProvider router={router} />
+    </>
   );
 }
 
